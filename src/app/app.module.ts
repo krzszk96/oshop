a9d5f7ab7c2e651f5699708d830b57ccdde73b28
krzysztof <krzszk96@gmail.com>
2022-05-23 23:29:12 +0200

    check out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,8 @@ import { ShoppingCartService } from './shopping-cart.service';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
+import { OrderService } from './order.service';
+import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
 
 @NgModule({
   declarations: [
@@ -53,7 +55,8 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
     ProductFormComponent,
     ProductFilterComponent,
     ProductCardComponent,
-    ProductQuantityComponent
+    ProductQuantityComponent,
+    ShoppingCartSummaryComponent
   ],
   imports: [
     BrowserModule,
@@ -77,7 +80,7 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
       {path:'login', component: LoginComponent},
 
       {path:'check-out', component: CheckOutComponent, canActivate:[AuthGuard]},
-      {path:'order-success', component: OrderSuccessComponent, canActivate:[AuthGuard]},
+      {path:'order-success/:id', component: OrderSuccessComponent, canActivate:[AuthGuard]},
       {path:'my/orders', component: MyOrdersComponent, canActivate:[AuthGuard]},
             
       {path:'admin/products/new', component: ProductFormComponent, canActivate:[AuthGuard, AdminAuthGuardService]},
@@ -94,7 +97,8 @@ import { ProductQuantityComponent } from './product-quantity/product-quantity.co
     AdminAuthGuardService,
     CategoryService,
     ProductService,
-    ShoppingCartService
+    ShoppingCartService,
+    OrderService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth.service';
+import { Order } from '../models/order';
+import { ShoppingCart } from '../models/shopping-cart';
+import { OrderService } from '../order.service';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy{
+  shipping:any = {}; 
+  cart: ShoppingCart;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
+  userId: string;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private shoppingCartService: ShoppingCartService, 
+    private orderService: OrderService){}
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart();
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription =  this.authService.user$.subscribe(user => this.userId = user.uid);
   }
+
+  ngOnDestroy() {
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
+  
+  async placeOrder() {
+    let order = new Order(this.userId, this.shipping, this.cart);
+    let result = await this.orderService.placeOrder(order);    
+    this.router.navigate(['/order-success', result.key]);
+  } 
 
 }
